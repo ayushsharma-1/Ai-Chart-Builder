d5aaeb0c2f685cd49211098e0dab3142f1531152
ayushsharma-1 <ayushsharma18001@gmail.com>
2026-06-10 09:37:35 +0530

    DB for ssl prod

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -13,6 +13,7 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
   connectTimeout: 10000,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
 });
 
 export default pool;
